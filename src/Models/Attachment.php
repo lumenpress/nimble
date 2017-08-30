e401bf579a6a3b059cf448fe58e7ca1f1b49ccb3
ChenL <chenlinxh@gmail.com>
2017-08-30 15:59:09 +0800

    Attachment

--- src/Models/Attachment.php
+++ src/Models/Attachment.php
@@ -2,7 +2,104 @@
 
 namespace Lumenpress\ORM\Models;
 
-class Attachment extends Post
+class Attachment extends AbstractPost
 {
     protected $type = 'attachment';
+
+    protected $appends = [
+    ];
+
+    protected $aliases = [
+        'link' => 'guid',
+    ];
+
+    protected $hidden = [
+        'guid',
+    ];
+
+    /**
+     * Accessor for link attribute.
+     *
+     * @return returnType
+     */
+    public function getGuidAttribute($value)
+    {
+        return $value ?: wp_get_attachment_url($this->ID);
+    }
+
+    private static function getAttachmentUniqid($value)
+    {
+        $meta = PostMeta::where('meta_key', '_lumenpress_asset_uniqid')->where('meta_value', $value)->first();
+        return $meta ? $meta->post_id : 0;
+    }
+
+    public static function upload($src, $force = false)
+    {
+        if (!$force and $id = static::getAttachmentUniqid($src)) {
+            return static::find($id);
+        }
+
+        // gives us access to the download_url() and wp_handle_sideload() functions
+        if (!function_exists('media_handle_upload')) {
+            require_once(ABSPATH . "wp-admin" . '/includes/image.php');
+            require_once(ABSPATH . "wp-admin" . '/includes/file.php');
+            require_once(ABSPATH . "wp-admin" . '/includes/media.php');
+        }
+
+        // URL to the WordPress logo
+        //$url = get_template_directory_uri() . '/' . $src;
+        //$tmp = download_url( $url );
+        $tmp = null;
+        if (lumenpress_is_url($src)) {
+            $tmp = download_url($src, 5000);
+        } else {
+            $url = file_exists($src) ? $src : lumenpress_asset_path($src);
+            $tmp = wp_tempnam($url);
+            d($url);
+            @copy($url, $tmp);
+        }
+        // clearing the stat cache
+        @clearstatcache(true, $tmp);
+        $file_array = array(
+            'name' => basename($src),
+            'tmp_name' => $tmp,
+        );
+
+        /**
+         * Check for download errors
+         * if there are error unlink the temp file name
+         */
+        if (is_wp_error($tmp)) {
+            @unlink($file_array['tmp_name']);
+            throw new \Exception($tmp->get_error_message(), 1);
+            return $tmp;
+        }
+
+        /**
+         * now we can actually use media_handle_sideload
+         * we pass it the file array of the file to handle
+         * and the post id of the post to attach it to
+         * $post_id can be set to '0' to not attach it to any particular post
+         */
+        $post_id = 0;
+
+        $id = media_handle_sideload($file_array, $post_id);
+
+        /**
+         * We don't want to pass something to $id
+         * if there were upload errors.
+         * So this checks for errors
+         */
+        if (is_wp_error($id)) {
+            @unlink($file_array['tmp_name']);
+            throw new \Exception($id->get_error_message(), 1);
+            return $id;
+        }
+
+        add_post_meta($id, '_lumenpress_asset_uniqid', $src, true);
+
+        @unlink($file_array['tmp_name']);
+
+        return static::find($id);
+    }
 }
